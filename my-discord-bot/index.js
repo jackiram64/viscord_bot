1621e3864e8d3f7164d6ef4333f050e40dce26a4
Jonathan Roemers <roemerj2@hva.nl>
2026-04-26 23:43:22 +0200

    the first

---
+++ my-discord-bot/index.js
@@ -0,0 +1,35 @@
+// Import required modules 
+const { Client, GatewayIntentBits } = require('discord.js'); 
+require('dotenv').config(); 
+
+// Create a new Discord client with message intent 
+const client = new Client({ 
+  intents: [ 
+      GatewayIntentBits.Guilds,  
+      GatewayIntentBits.GuildMessages,  
+      GatewayIntentBits.MessageContent] 
+}); 
+
+// Bot is ready 
+client.once('ready', () => { 
+  console.log(`🤖 Logged in as ${client.user.tag}`); 
+}); 
+
+// Listen and respond to messages 
+client.on('messageCreate', message => { 
+
+  // Ignore messages from bots 
+  if (message.author.bot) return; 
+
+  // Respond to a specific message 
+  if (message.content.toLowerCase() === 'hi discord mod') { 
+    message.reply('Hi there! I am Discord mod :D . I never take showers and leave all my hairs behind in the kitchien :)'); 
+  }
+  
+    if (message.content.toLowerCase() === 'pannenkoek') { 
+    message.reply('I love '); 
+  }
+});   
+
+// Log in to Discord using token from .env 
+client.login(process.env.DISCORD_TOKEN); 
